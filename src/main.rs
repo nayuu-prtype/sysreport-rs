944064cefaf0fd7979d9afb375c3ab4c1324041a
nayuu <lucasdaniel.prtype@proton.me>
2026-06-25 04:15:31 -0300

    prototype 2.0

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use sysinfo::System;
+use sysinfo::{System, Components};
+
+const BYTES_IN_GIGABYTES: f64 = 1024.0 * 1024.0 * 1024.0;
 
 fn main() {
     println!("telemetric script initialized");
@@ -8,31 +10,51 @@ fn main() {
 }
 
 fn system_info() -> String {
+
+    // refresh system
     let mut sys = System::new_all();
+    let components = Components::new_with_refreshed_list();
     sys.refresh_all();
 
-    let hostname = System::host_name();
-    let system = System::name();
-    let 
-    
-    let temperature = 
+    let hostname = System::host_name().unwrap_or("failed".to_string());
+    let system = System::name().unwrap_or("failed".to_string());
+    let kernel_version = System::kernel_version().unwrap_or("failed".to_string());
+
+    // cpu usage
     let cpu_usage = sys.global_cpu_usage();
 
-    let ram_total = sys.total_memory();
-    let ram_used = sys.used_memory();
-    let swap_total = sys.total_swap();
-    let swap_used = sys.used_swap();
+    // ram usage 
+    let ram_total: f64 = (sys.total_memory() as f64) / BYTES_IN_GIGABYTES;
+    let ram_used: f64 = (sys.used_memory() as f64) / BYTES_IN_GIGABYTES;
+    let swap_total: f64 = (sys.total_swap() as f64) / BYTES_IN_GIGABYTES;
+    let swap_used: f64 = (sys.used_swap() as f64) / BYTES_IN_GIGABYTES;
 
-    let relatory = "
-        Telemetry script informations:
-        Software:
-        hostname = {hostname}
+    // temperature
+    let mut temperature_cpu = String::new();
+    for component in &components {
+        if let Some(temperature) = component.temperature()  {
+            temperature_cpu = temperature.to_string();
+        } else {
+            temperature_cpu = "unknow".to_string();
+        }
+    }
+
+    let relatory = format!("
+        Telemetry script:
+
+        ┌ Software ─┐
+        │hostname   │   {hostname}
+        │system            {system}
+        │kernel         {kernel_version}
 
         Hardware:
-        cpu usage {cpu_usage} 
-        ram  {ram_used} / {ram_total} bytes
-        swap {swap_used} / {swap_total} bytes
-        "
+        cpu usage         {cpu_usage:.2} 
+        ram usage         {ram_used:.2}/{ram_total:.2}GB
+        swap usage        {swap_used:.2}/{swap_total:.2}GB
+        temperature       {temperature_cpu} °C
+        ");
+
+        return relatory;
 }
 
 fn telemetric_send(relatory: &str) {
